b28352ac0f8d3a20a37f84fabf237bcd702d7253
Kleber <kleberkapelinski@gmail.com>
2022-09-30 22:18:24 -0300

    Adding more examples.

---
+++ ex2-simpleUI/main.cpp
@@ -0,0 +1,31 @@
+#include "AppToolkit.h"
+
+//-------------------------------------------------------------------
+
+class HelloWorldUI :
+  /*implements*/ public AppToolkit::IuiPresenter
+{
+  public:
+    void Present() override
+    {
+      ImGui::Begin("Hello world");
+      ImGui::Text("HellooOoOo \\o/");
+      ImGui::End();
+    }
+};
+
+//-------------------------------------------------------------------
+
+int main() {
+  AppToolkit::IApp& app = AppToolkit::GetApp();
+
+  HelloWorldUI helloUI;
+  app.SetUiPresenter(&helloUI);
+
+  app.Initialize();
+  app.Run();
+  
+  return 0;
+}
+
+//-------------------------------------------------------------------
---
+++ ex3-drawing/main.cpp
@@ -0,0 +1,33 @@
+#include "AppToolkit.h"
+
+//-------------------------------------------------------------------
+
+class SimpleTriangleDrawer :
+  /*implements*/ public AppToolkit::IAppRenderer
+{
+  public:
+    void Render() override
+    {
+      glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(1.0f, 0.0f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(0.0f, 1.0f, 0.0f);
+      glEnd();
+    }
+};
+
+//-------------------------------------------------------------------
+
+int main() {
+  AppToolkit::IApp& app = AppToolkit::GetApp();
+
+  SimpleTriangleDrawer drawer;
+  app.SetAppRenderer(&drawer);
+  
+  app.Initialize();
+  app.Run();
+  
+  return 0;
+}
+
+//-------------------------------------------------------------------
